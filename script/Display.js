72df7402ecd225a50439c202d811a770f8fc26a1
Ivan Consuegra <ie.consuegra@gmail.com>
2020-10-17 21:08:19 -0500

    Added a class to process all canvas related logic * Display.js

---
+++ script/Display.js
@@ -0,0 +1,48 @@
+
+class Display {
+  constructor(width, height) {
+    this.canvas = document.querySelector('canvas');
+    this.canvasCtx = this.canvas.getContext('2d');
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.canvasHalfWidth = width / 2;
+    this.canvasHalfHeight = height / 2;
+    this.canvasBackgroundColor = '#222';
+    this.canvasElementsColor = '#FFF';
+    this.draw = this.draw.bind(this);
+    this.net = {};
+  }
+
+  render(elementsArr) {
+    this.elementsArr = [...elementsArr];
+    // Background default
+    this.canvasCtx.fillStyle = this.canvasBackgroundColor;
+    this.canvasCtx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    // Color of the game elements
+    this.canvasCtx.fillStyle = this.canvasElementsColor;
+
+    // Render the net
+    this.drawNet();
+    // Iterates the array and render each element
+    this.elementsArr.forEach(this.draw);
+  }
+
+  draw(element) {
+    this.canvasCtx.fillRect(element.posX, element.posY, element.width, element.height);
+  }
+
+  drawNet() {
+    this.net.gapDelimiter = this.canvas.height / 32;
+    this.net.width = 3;
+    this.net.height = this.net.gapDelimiter - Math.floor(this.canvas.height / 72);
+
+    for (let index = 0; index < this.canvas.height; index += this.net.gapDelimiter) {
+      this.net.posX = this.canvasHalfWidth - Math.floor(this.net.width / 2);
+      this.net.posY = index;
+      this.draw(this.net);
+    }
+  }
+}
+
+export default Display;
--- script/main.js
+++ script/main.js
@@ -1,61 +1,28 @@
+import Display from './Display.js';
 import Player from './Player.js';
 import Ball from './Ball.js';
 import Controller from './Controller.js'
 
-
 // import SoundGen from './sound';
 
-// TV
-const screen = document.querySelector('canvas');
-const ctxScreen = screen.getContext('2d');
-const screenWidth = screen.width;
-const screenHeight = screen.height;
-const screenYHalf = screenHeight / 2;
-const screenXHalf = screenWidth / 2;
-const elementsOnScreenColor = '#FFF';
-const backgroundScreenColor = '#222';
 const playerFrameGap = 20;
 
+const display = new Display(600, 400);
+
 // Create players
-const player1 = new Player(playerFrameGap, screenYHalf - 40);
-const player2 = new Player(screenWidth - 40, screenYHalf - 40);
+const player1 = new Player(playerFrameGap, display.canvasHalfHeight - 40);
+const player2 = new Player(display.canvas.width - 40, display.canvasHalfHeight - 40);
+player1.hasAI = true;
 player2.hasAI = true;
 
 // Ball
 const ball = new Ball(50, 180);
 
-function render() {
-  // Background
-  ctxScreen.fillStyle = backgroundScreenColor;
-  ctxScreen.fillRect(0, 0, screenWidth, screenHeight);
-
-  // Define the elements on screen color
-  ctxScreen.fillStyle = elementsOnScreenColor;
-
-  // Net, created by a line made by 32 dashes
-  const gapDelimiter = screenHeight / 32;
-  const netWidth = 3;
-  const netHeight = gapDelimiter - Math.floor(screenHeight / 72);
-  for (let index = 0; index < screenHeight; index += gapDelimiter) {
-    ctxScreen.fillRect(screenXHalf - netWidth / 2, index, netWidth, netHeight);
-  }
-
-  // Players
-
-  ctxScreen.fillRect(player1.posX, player1.posY, player1.width, player1.height);
-
-  ctxScreen.fillRect(player2.posX, player2.posY, player2.width, player2.height);
-
-  // Ball
-  ctxScreen.fillRect(ball.posX, ball.posY, ball.width, ball.height);
-}
-
 setInterval(() => {
-  // ballMove();
   ball.update([player1, player2]);
-  player1.update();
+  player1.update(ball);
   player2.update(ball);
-  render();
+  display.render([ball, player1, player2]);
 }, 20);
 
 const controller = new Controller();
